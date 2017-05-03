b14e7d5f37c1bfb291628cf4352b2768a6286fda
taha zayed <tahazayed@gmail.com>
2017-05-03 07:29:41 +0200

    2017-05-03

--- cookpad/settings.py
+++ cookpad/settings.py
@@ -101,7 +101,7 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = False
 HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = './httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
