940bd263dcb0a77307eb8d9d44fa2d68d275e843
taha zayed <tahazayed@gmail.com>
2017-05-09 15:29:52 +0200

    2017-05-09

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'cookpad',
-    version      = '1.0',
+    version      = '1.1',
     packages     = find_packages(),
     install_requires=[
         'distribute',
@@ -10,6 +10,7 @@ setup(
         'psycopg2',
         'bs4',
         'pymongo',
+		'pymssql'
     ],
     entry_points = {'scrapy': ['settings = cookpad.settings']},
     scripts = ['bin/testargs.py']
