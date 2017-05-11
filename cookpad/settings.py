3addfe8a17ee1657c2fdd164c4f05a8f8284db84
taha zayed <tahazayed@gmail.com>
2017-05-11 22:36:25 +0200

    2017-05-11

--- cookpad/settings.py
+++ cookpad/settings.py
@@ -17,7 +17,7 @@ LOG_LEVEL = 'ERROR'
 #LOG_FILE = 'cookpad.log'
 
 
-IS_MSSQLDB = False
+IS_MSSQLDB = True
 
 ITEM_PIPELINES = {'cookpad.pipelines.MsSQLDBPipeline':300}
 
@@ -109,7 +109,7 @@ AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = False
+HTTPCACHE_ENABLED = True
 HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = './httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
