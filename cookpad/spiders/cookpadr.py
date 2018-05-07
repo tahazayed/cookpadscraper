c5fcf0274670f2dafe268b93bd9cd09d09160faf
Taha Amin <taha_zayed@hotmail.com>
2018-05-08 00:48:32 +0200

    2018-05-08

--- cookpad/spiders/cookpadr.py
+++ cookpad/spiders/cookpadr.py
@@ -164,8 +164,8 @@ runner = CrawlerProcess(settings=project_settings)
 
 @defer.inlineCallbacks
 def crawl():
-    #yield runner.crawl(ExtractlinksSpider)
-    yield runner.crawl(CookpadrSpider)
+    yield runner.crawl(ExtractlinksSpider)
+    #yield runner.crawl(CookpadrSpider)
     reactor.stop()
 
 
