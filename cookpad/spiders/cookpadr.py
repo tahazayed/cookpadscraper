255ab845e8a6f5a832da4a36ad1ad1bca5721e66
Taha Amin <taha_zayed@hotmail.com>
2018-05-15 01:00:06 +0200

    2018-05-15

--- cookpad/spiders/cookpadr.py
+++ cookpad/spiders/cookpadr.py
@@ -40,8 +40,8 @@ class CookpadrSpider(scrapy.Spider):
     def parse(self, response):
         page = response.body.decode("utf-8")
         tempSoup = BeautifulSoup(page, 'lxml')
-        pre  = tempSoup.find('pre').contents[0]
-    
+        pre  = tempSoup.find('pre')
+        self.logger.debug(pre)    
         
         soup = BeautifulSoup(pre, 'html.parser')
 
